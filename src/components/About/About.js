c68bbb3a3d9e7b86cf65f57cf8d0a4d4f1c32404
Rutito <kakaroto11@live.com.ar>
2022-11-24 14:40:49 -0300

    ahh

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -41,160 +41,145 @@ import "../../css/inner.css";
 import $ from "jquery";
 
 export default function About() {
-    useEffect(() => {
-        AOS.init();
-    }, []);
+  useEffect(() => {
+    AOS.init();
+  }, []);
 
-    // window.onload = loaded();
-    $(window).trigger("scroll", scrolled());
+  // window.onload = loaded();
+  $(window).trigger("scroll", scrolled());
 
-    const [payType, setType] = useState("none");
+  const [payType, setType] = useState("none");
 
-    const handleClick = (e, type) => {
-        e.preventDefault();
-        if (payType === type) {
-            setType("none");
-        } else {
-            setType(type);
-        }
-    };
+  const handleClick = (e, type) => {
+    e.preventDefault();
+    if (payType === type) {
+      setType("none");
+    } else {
+      setType(type);
+    }
+  };
 
-    return (
-        <div>
-            {/*loading*/}
-            <Loading />
-            {/*end loading*/}
-            <div className="page">
-                {/*social*/}
-                <Social />
-                {/*end social*/}
+  return (
+    <div>
+      {/*loading*/}
+      <Loading />
+      {/*end loading*/}
+      <div className="page">
+        {/*social*/}
+        <Social />
+        {/*end social*/}
 
-                {/*servers*/}
-                <Servers />
-                {/*end servers*/}
+        {/*servers*/}
+        <Servers />
+        {/*end servers*/}
 
-                {/*logo*/}
-                <div className="logo-header">
-                    <img src={imagesLogo} alt="../../images/logo.png" />
-                </div>
-                {/*nav*/}
-                <Nav page="About" />
-                {/*end nav*/}
-
-                <SparklesEffect />
-
-                {/*inner content*/}
-                <section className="inner container">
-                    <div className="line"></div>
-                    <div className="inner-title">
-                        About: <span>Infinity Essence</span>
-                    </div>
-                    <div className="inner-content">
-                        {/*topic 1*/}
-                        <div id="topic1" className="topic">
-                            <h1>
-                                <img
-                                    src={imagesSymbol}
-                                    alt="../../images/symbol.png"
-                                />
-                                The brand new Infinity Essence!{" "}
-                            </h1>
-                            <img src={banner1} alt="../../images/banner/1.jpg" />
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing
-                                elit. Vel unde, facilis voluptatum possimus fuga enim
-                                dolores consequatur quia voluptates ad numquam magni
-                                itaque, adipisci quas alias sunt asperiores quae
-                                magnam?
-                            </p>
-                            <p>
-                                <strong>
-                                    Lorem ipsum dolor sit amet consectetur
-                                    adipisicing elit. Corrupti, minima!
-                                </strong>
-                            </p>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing
-                                elit. Nulla voluptatibus, ex ipsam, velit eum quis id
-                                reprehenderit officiis vel ratione debitis incidunt,
-                                ipsa reiciendis repudiandae quia possimus? Amet
-                                earum, iure molestiae laborum consectetur totam
-                                aliquam laudantium quae modi neque placeat
-                                consequatur est et nam, officiis asperiores
-                                perferendis ipsa distinctio maiores? Eum, nam?
-                                Laudantium vel earum tempore inventore nam voluptates
-                                sapiente natus, explicabo magni necessitatibus fugiat
-                                expedita corrupti iste eum voluptatum dolore laborum
-                                nihil id atque adipisci cum harum. Beatae, obcaecati.
-                            </p>
-                        </div>
-                        {/*topic 2*/}
-                        <div id="topic2" className="topic">
-                            <h1>
-                                <img
-                                    src={imagesSymbol}
-                                    alt="../../images/symbol.png"
-                                />
-                                Donation
-                            </h1>
-
-                            <div>
-                                <div className="table">
-                                    <div>
-                                        {payType !== "PayPal" ? (
-                                            <button
-                                                className="title"
-                                                onClick={(e) =>
-                                                    handleClick(e, "PayPal")
-                                                }
-                                            >
-                                                PayPal
-                                            </button>
-                                        ) : (
-                                            <PayWPayPal />
-                                        )}
-                                    </div>
-                                </div>
-                                <div className="table">
-                                    <div>
-                                        {payType !== "Mercado Pago" ? (
-                                            <button
-                                                className="title"
-                                                onClick={(e) =>
-                                                    handleClick(e, "Mercado Pago")
-                                                }
-                                            >
-                                                Mercado Pago
-                                            </button>
-                                        ) : (
-                                            <PayWMPago />
-                                        )}
-                                    </div>
-                                </div>
-                                <div className="table">
-                                    <div>
-                                        {payType !== "Stripe" ? (
-                                            <button
-                                                className="title"
-                                                onClick={(e) =>
-                                                    handleClick(e, "Stripe")
-                                                }
-                                            >
-                                                Stripe
-                                            </button>
-                                        ) : (
-                                            <PayWStripe />
-                                        )}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            </div>
-            {/*footer*/}
-            <Footer />
-            {/*end footer*/}
+        {/*logo*/}
+        <div className="logo-header">
+          <img src={imagesLogo} alt="../../images/logo.png" />
         </div>
-    );
+        {/*nav*/}
+        <Nav page="About" />
+        {/*end nav*/}
+
+        <SparklesEffect />
+
+        {/*inner content*/}
+        <section className="inner container">
+          <div className="line"></div>
+          <div className="inner-title">
+            About: <span>Infinity Essence</span>
+          </div>
+          <div className="inner-content">
+            {/*topic 1*/}
+            <div id="topic1" className="topic">
+              <h1>
+                <img src={imagesSymbol} alt="../../images/symbol.png" />
+                The brand new Infinity Essence!{" "}
+              </h1>
+              <img src={banner1} alt="../../images/banner/1.jpg" />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
+                unde, facilis voluptatum possimus fuga enim dolores consequatur
+                quia voluptates ad numquam magni itaque, adipisci quas alias
+                sunt asperiores quae magnam?
+              </p>
+              <p>
+                <strong>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Corrupti, minima!
+                </strong>
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+                voluptatibus, ex ipsam, velit eum quis id reprehenderit officiis
+                vel ratione debitis incidunt, ipsa reiciendis repudiandae quia
+                possimus? Amet earum, iure molestiae laborum consectetur totam
+                aliquam laudantium quae modi neque placeat consequatur est et
+                nam, officiis asperiores perferendis ipsa distinctio maiores?
+                Eum, nam? Laudantium vel earum tempore inventore nam voluptates
+                sapiente natus, explicabo magni necessitatibus fugiat expedita
+                corrupti iste eum voluptatum dolore laborum nihil id atque
+                adipisci cum harum. Beatae, obcaecati.
+              </p>
+            </div>
+            {/*topic 2*/}
+            <div id="topic2" className="topic">
+              <h1>
+                <img src={imagesSymbol} alt="../../images/symbol.png" />
+                Donation
+              </h1>
+
+              <div>
+                <div className="table">
+                  <div>
+                    {payType !== "PayPal" ? (
+                      <button
+                        className="title"
+                        onClick={(e) => handleClick(e, "PayPal")}
+                      >
+                        PayPal
+                      </button>
+                    ) : (
+                      <PayWPayPal />
+                    )}
+                  </div>
+                </div>
+                <div className="table">
+                  <div>
+                    {payType !== "Mercado Pago" ? (
+                      <button
+                        className="title"
+                        onClick={(e) => handleClick(e, "Mercado Pago")}
+                      >
+                        Mercado Pago
+                      </button>
+                    ) : (
+                      <PayWMPago />
+                    )}
+                  </div>
+                </div>
+                <div className="table">
+                  <div>
+                    {payType !== "Stripe" ? (
+                      <button
+                        className="title"
+                        onClick={(e) => handleClick(e, "Stripe")}
+                      >
+                        Stripe
+                      </button>
+                    ) : (
+                      <PayWStripe />
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      {/*footer*/}
+      <Footer />
+      {/*end footer*/}
+    </div>
+  );
 }
--- src/components/Forms/Donations/PayPal.js
+++ src/components/Forms/Donations/PayPal.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 
 import { PayPalButton } from "react-paypal-button-v2";
@@ -8,152 +8,175 @@ import swal from "sweetalert";
 import { paywPayPal } from "../../../redux/actions";
 
 export default function PayWPayPal() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const [donationAmountPayPal, setDonationAmountPayPal] = useState(1);
+  const [donationAmountPayPal, setDonationAmountPayPal] = useState(1);
+  const [render, setRender] = useState("hidden");
+  const [formpp, setForm] = useState({
+    amount: "",
+    email: "",
+  });
 
-    const [formpp, setForm] = useState({
-        amount: "",
-        email: "",
+  const validateForm = (formpp) => {
+    const email = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]+$/;
+    const amount = /^[0-9]+$/;
+
+    const errors = {
+      email: false,
+      amount: false,
+    };
+
+    if (!email.test(formpp.email.toLowerCase().trim())) {
+      errors.email = true;
+    } else {
+      errors.email = false;
+    }
+    if (!amount.test(formpp.amount.trim())) {
+      errors.amount = true;
+    } else {
+      errors.amount = false;
+    }
+
+    return errors;
+  };
+  useEffect(() => {
+    setTimeout(() => setRender("visible"), 1000);
+  });
+  //   useEffect(
+  //     () => {
+  //       let timer1 = setTimeout(() => setRender("visible"), delay * 1000);
+
+  //       // this will clear Timeout
+  //       // when component unmount like in willComponentUnmount
+  //       // and show will not change to true
+  //       return () => {
+  //         clearTimeout(timer1);
+  //       };
+  //     },
+  //     // useEffect will run only one time with empty []
+  //     // if you pass a value to array,
+  //     // like this - [data]
+  //     // than clearTimeout will run every time
+  //     // this value changes (useEffect re-run)
+  //     []
+  //   );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const errors = validateForm(formpp);
+
+    if (!formpp.amount || !formpp.email) {
+      swal({
+        title: "Faltan datos",
+        icon: "error",
+        button: "Aceptar",
+        timer: "10000",
+      });
+    } else if (errors.email === true) {
+      swal({
+        title: "Debe ser un correo/email valido",
+        text: "Solo puede contener letras, numeros, puntos, guiones y guion bajo",
+        icon: "error",
+        button: "Aceptar",
+        timer: "10000",
+      });
+    } else if (errors.amount === true) {
+      swal({
+        // title: "Debe ser un correo valida",
+        text: "La cantidad a donar debe ser solo en numeros",
+        icon: "error",
+        button: "Aceptar",
+        timer: "10000",
+      });
+    } else {
+      // algo@algo.com
+      dispatch(paywPayPal(formpp));
+    }
+  };
+
+  const handleInput = (e) => {
+    e.preventDefault();
+
+    setForm({
+      ...formpp,
+      [e.target.name]: e.target.value,
     });
 
-    const validateForm = (formpp) => {
-        const email = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]+$/;
-        const amount = /^[0-9]+$/;
+    if (e.target.name === "amount") setDonationAmountPayPal(e.target.value);
+  };
 
-        const errors = {
-            email: false,
-            amount: false,
-        };
+  try {
+    return (
+      <div
+        style={{
+          marginTop: 20,
+        }}
+      >
+        <h2> PayPal </h2>
+        <form
+          className="create-div-container"
+          onSubmit={(e) => handleSubmit(e)}
+        >
+          <input
+            className="from-title"
+            name="amount"
+            type="number"
+            value={formpp.amount}
+            placeholder=" Amount..."
+            onChange={(e) => handleInput(e)}
+          />
+          {/* <hr /> */}
+          <input
+            name="email"
+            type="email"
+            value={formpp.email}
+            placeholder=" user@mail.com"
+            onChange={(e) => handleInput(e)}
+          />
 
-        if (!email.test(formpp.email.toLowerCase().trim())) {
-            errors.email = true;
-        } else {
-            errors.email = false;
-        }
-        if (!amount.test(formpp.amount.trim())) {
-            errors.amount = true;
-        } else {
-            errors.amount = false;
-        }
+          {/* <hr /> */}
+          {render === "hidden" ? (
+            <span></span>
+          ) : (
+            <span
+              style={{
+                maxHeight: 120,
+                maxWidth: 250,
 
-        return errors;
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        const errors = validateForm(formpp);
-
-        if (!formpp.amount || !formpp.email) {
-            swal({
-                title: "Faltan datos",
-                icon: "error",
-                button: "Aceptar",
-                timer: "10000",
-            });
-        } else if (errors.email === true) {
-            swal({
-                title: "Debe ser un correo/email valido",
-                text: "Solo puede contener letras, numeros, puntos, guiones y guion bajo",
-                icon: "error",
-                button: "Aceptar",
-                timer: "10000",
-            });
-        } else if (errors.amount === true) {
-            swal({
-                // title: "Debe ser un correo valida",
-                text: "La cantidad a donar debe ser solo en numeros",
-                icon: "error",
-                button: "Aceptar",
-                timer: "10000",
-            });
-        } else {
-            // algo@algo.com
-            dispatch(paywPayPal(formpp));
-        }
-    };
-
-    const handleInput = (e) => {
-        e.preventDefault();
-
-        setForm({
-            ...formpp,
-            [e.target.name]: e.target.value,
-        });
-
-        if (e.target.name === "amount") setDonationAmountPayPal(e.target.value);
-    };
-
-    try {
-        return (
-            <div
-                style={{
-                    marginTop: 20,
-                }}
+                // maxWidth: 100,
+                // overflow: "hidden",
+                paddingTop: -30,
+                padding: 0,
+                float: "right",
+                marginTop: -30,
+                // marginLeft: -70,
+              }}
             >
-                <h2> PayPal </h2>
-                <form
-                    className="create-div-container"
-                    onSubmit={(e) => handleSubmit(e)}
-                >
-                    <input
-                        className="from-title"
-                        name="amount"
-                        type="number"
-                        value={formpp.amount}
-                        placeholder=" Amount..."
-                        onChange={(e) => handleInput(e)}
-                    />
-                    {/* <hr /> */}
-                    <input
-                        name="email"
-                        type="email"
-                        value={formpp.email}
-                        placeholder=" user@mail.com"
-                        onChange={(e) => handleInput(e)}
-                    />
+              <PayPalButton
+                style={{ padding: 0 }}
+                amount={donationAmountPayPal}
+                // shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
+                onSuccess={(details, data) => {
+                  console.log(formpp);
+                  // OPTIONAL: Call your server to save the transaction
 
-                    {/* <hr /> */}
-
-                    <span
-                        style={{
-                            maxHeight: 120,
-                            maxWidth: 250,
-                            // maxWidth: 100,
-                            // overflow: "hidden",
-                            paddingTop: -30,
-                            padding: 0,
-                            float: "right",
-                            marginTop: -30,
-                            // marginLeft: -70,
-                        }}
-                    >
-                        <PayPalButton
-                            style={{ padding: 0 }}
-                            amount={donationAmountPayPal}
-                            // shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
-                            onSuccess={(details, data) => {
-                                console.log(formpp);
-                                // OPTIONAL: Call your server to save the transaction
-
-                                dispatch(paywPayPal(formpp));
-                                setTimeout(() => {
-                                    window.location.href =
-                                        "http://localhost:3000/success";
-                                }, 1000);
-                            }}
-                            options={{
-                                clientId:
-                                    "AcsldefjQXoml-Leg34FhsFvCMYZ-_eseJP0-UBaZ3iPIfo-FxfM4nQ6MAwJKJ5iMylMHnsVDcIZQp2r",
-                                // disableFunding: "credit,card",
-                            }}
-                        />
-                    </span>
-                </form>
-            </div>
-        );
-    } catch (error) {
-        console.log(error);
-    }
+                  dispatch(paywPayPal(formpp));
+                  setTimeout(() => {
+                    window.location.href = "http://localhost:3000/success";
+                  }, 1000);
+                }}
+                options={{
+                  clientId:
+                    "AcsldefjQXoml-Leg34FhsFvCMYZ-_eseJP0-UBaZ3iPIfo-FxfM4nQ6MAwJKJ5iMylMHnsVDcIZQp2r",
+                  // disableFunding: "credit,card",
+                }}
+              />
+            </span>
+          )}
+        </form>
+      </div>
+    );
+  } catch (error) {
+    console.log(error);
+  }
 }
