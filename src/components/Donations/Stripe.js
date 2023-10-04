93ad41c9ecf82773fdd66119c50839139785649a
Ianina Mia Passalia <miau.passalia99@hotmail.com>
2023-10-04 11:20:58 -0300

    Update Stripe.js

--- src/components/Donations/Stripe.js
+++ src/components/Donations/Stripe.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+/* import React, { useState, useEffect } from "react";
 
 import "../../css/donations.css";
 
@@ -84,3 +84,4 @@ export default function PayWStripe() {
 
     return message ? <Message message={message} /> : <ProductDisplay />;
 }
+*/
